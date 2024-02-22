325fe67282a8d1319dd65d6961e07882c9ffa897
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-02-22 15:22:51 +0900

    Create Revising the Select Query I.sql

---
+++ SQL/01_Basic Select/01_Revising the Select Query/Revising the Select Query I.sql
@@ -0,0 +1,23 @@
+/*
+Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+
+The CITY table is described as follows:
++-------------+--------------+
+| Feild       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(3)  |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+*/
+
+-- MySQL--
+SELECT
+    *
+FROM
+    CITY
+WHERE
+    POPULATION >= 100000
+    AND COUNTRYCODE = 'USA'
