615341d8b74fb88d73451ee670b208b88fba8886
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-02-22 15:32:02 +0900

    Update Revising the Select Query I.sql

--- SQL/01_Basic Select/01_Revising the Select Query/Revising the Select Query I.sql
+++ SQL/01_Basic Select/01_Revising the Select Query/Revising the Select Query I.sql
@@ -4,7 +4,7 @@ Query all columns for all American cities in the CITY table with populations lar
 
 The CITY table is described as follows:
 +-------------+--------------+
-| Feild       | Type         |
+| Field       | Type         |
 +-------------+--------------+
 | ID          | NUMBER       |
 | NAME        | VARCHAR2(17) |
